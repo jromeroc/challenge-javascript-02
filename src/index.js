030adb8ee5334cc42ec5f6add07f991ddae6bd4e
jromeroc <jcamiloromeroc@gmail.com>
2020-08-11 19:59:01 -0500

    Done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const fibonacci = (n) => {
-
+  const sequence = [0, 1];
+  for (let i = 0; i < n - 1; i++) {
+    let numCalc = 0;
+    numCalc = sequence[i] + sequence[i + 1];
+    sequence.push(numCalc);
   }
+  sequence.shift();
+  return sequence;
+}
 
 
-  module.exports = fibonacci;
+module.exports = fibonacci;
